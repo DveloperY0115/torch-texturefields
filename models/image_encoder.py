87a019e865679ef1b146bc35f53842a45ff7cdf0
SeungwooYoo <dreamy1534@kaist.ac.kr>
2021-07-13 01:23:22 +0000

    feat(main): Unfreeze parameters of image encoder

--- models/image_encoder.py
+++ models/image_encoder.py
@@ -28,14 +28,8 @@ class TextureFieldsImageEncoder(nn.Module):
         self.model = models.resnet18(pretrained=True, progress=True)
         self.model.fc = nn.Sequential()  # ResNet-18 originally outputs score for 1000 classes
 
-        # freeze parameters of the network
-        for param in self.parameters():
-            param.requires_grad = False
-
         if use_linear:
             self.model.fc = nn.Linear(512, c_dim)
-            for param in self.model.fc.parameters():
-                param.requires_grad = True
         elif c_dim == 512:
             pass  # do nothing
         else:
@@ -58,7 +52,7 @@ class TextureFieldsImageEncoder(nn.Module):
 
 
 def normalize_imagenet(x):
-    x = x.clone()
+    # x = x.clone()
     x[:, 0] = (x[:, 0] - 0.485) / 0.229
     x[:, 1] = (x[:, 1] - 0.456) / 0.224
     x[:, 2] = (x[:, 2] - 0.406) / 0.225

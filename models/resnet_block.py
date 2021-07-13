719aff52e6bd895e04c0ee19bd17b304fbc1f632
SeungwooYoo <dreamy1534@kaist.ac.kr>
2021-07-13 05:33:30 +0000

    feat(main): Add class 'TextureFieldsResNetBlockPointwise'

--- models/resnet_block.py
+++ models/resnet_block.py
@@ -42,3 +42,40 @@ class TextureFieldsResNetBlock(nn.Module):
         x = self.maxpool(x)
 
         return x
+
+
+class TextureFieldsResNetBlockPointwise(nn.Module):
+    def __init__(self, in_dim, out_dim, hidden_dim, factor=1):
+        """
+        Constructor of TextureFieldsResNetBlockPointwise
+
+        Args:
+        - in_dim (int): Dimensionality of input feature vector.
+        - out_dim (int): Dimensionality of output feature vector.
+        - hidden_dim (int): Dimensionality of hidden feature vector within this ResNet block.
+        - factor (float): Weight for skip connection.
+        """
+        super(TextureFieldsResNetBlockPointwise, self).__init__()
+
+        self.factor = factor
+
+        self.conv_1 = nn.Conv1d(in_dim, hidden_dim, 1)
+        self.conv_2 = nn.Conv1d(hidden_dim, out_dim, 1)
+
+        if in_dim == out_dim:
+            self.shortcut = nn.Sequential()
+        else:
+            self.shortcut = nn.Conv1d(in_dim, out_dim, 1)
+
+    def forward(self, x):
+        """
+        Forward propagation.
+
+        Args:
+        - x (torch.Tensor): Tensor of shape 
+        """
+        skip = x.clone()
+        x = F.relu(self.conv_1(x))
+        x = self.conv_2(x)
+        skip = self.shortcut(skip)
+        return F.relu(skip + self.factor * x)

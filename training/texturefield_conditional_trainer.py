7eceea8789aa28787f594701ab01068096d8476f
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-02-17 07:39:23 +0000

    Add method 'inference'

--- training/texturefield_conditional_trainer.py
+++ training/texturefield_conditional_trainer.py
@@ -3,7 +3,7 @@ texturefield_conditional_trainer.py - Trainer for TextureFields conditional mode
 """
 
 import os
-import abc
+from PIL import Image
 from typing import Dict, Iterable, Tuple
 from collections import namedtuple
 from tqdm import tqdm
@@ -149,28 +149,96 @@ class TextureFieldsConditionalTrainer(BaseTrainer):
                 if self.opts.use_multi_gpu:
                     loss = loss.mean()
 
-            test_loss += loss.item()
+                test_loss += loss.item()
+    
+                assert gen_imgs is not None, "[!] Set of predicted images must not be empty."
 
-        assert gen_imgs is not None, "Set of predicted images must not be empty."
+                if self.opts.log_wandb:
+                    # log RGB images
+                    gen_imgs = gen_imgs.permute(0, 2, 3, 1).cpu().numpy()
+                    img = img.permute(0, 2, 3, 1).cpu().numpy()
+                    condition_img = condition_img.permute(0, 2, 3, 1).cpu().numpy()
 
-        if self.opts.log_wandb:
-            gen_imgs = gen_imgs.permute(0, 2, 3, 1).cpu().numpy()
-            img = img.permute(0, 2, 3, 1).cpu().numpy()
-            depth[torch.isinf(depth)] = 0
-            depth = depth.permute(0, 2, 3, 1).cpu().numpy()
-            condition_img = condition_img.permute(0, 2, 3, 1).cpu().numpy()
+                    # log depth maps
+                    depth[torch.isinf(depth)] = 0
+                    depth /= depth.max()
+                    depth *= 255.0
+                    depth = depth.type(torch.uint8)
+                    depth = depth.permute(0, 2, 3, 1).cpu().numpy()
 
-            log_dict = {
-                "Images/GT": [wandb.Image(x) for x in img],
-                "Images/Predicted-Generated": [wandb.Image(x) for x in gen_imgs],
-                "Images/Depth": [wandb.Image(x) for x in depth],
-            }
+                    log_dict = {
+                        "Images/GT": [wandb.Image(x) for x in img],
+                        "Images/Predicted-Generated": [wandb.Image(x) for x in gen_imgs],
+                        "Images/Depth": [wandb.Image(x) for x in depth],
+                        "Images/Condition": [wandb.Image(x) for x in condition_img]
+                    }
 
-            wandb.log(log_dict, step=self.epoch)
+                    wandb.log(log_dict, step=self.epoch)
 
         test_loss /= self.opts.test_dataset_size
 
         return test_loss
+
+    def inference(self, out_dir: str):
+        print("[!] Begin inference..")
+
+        if not os.path.exists(out_dir):
+            print("[!] Directory %s does not exist." % out_dir)
+            os.mkdir(out_dir)
+            os.mkdir(os.path.join(out_dir, "real"))
+            os.mkdir(os.path.join(out_dir, "fake"))
+            print("[!] Created directory %s." % out_dir)   
+
+        inf_total_loss = 0.0
+        sample_idx = 0
+
+        with torch.no_grad():
+            for test_batch in self.test_loader:
+                img, depth, camera_params, condition_img, pointcloud = test_batch
+                cam_K = camera_params["K"]
+                cam_R = camera_params["Rt"]
+
+                # send data to device
+                img = img.to(self.device).float()
+                depth = depth.to(self.device).float()
+                cam_K = cam_K.to(self.device).float()
+                cam_R = cam_R.to(self.device).float()
+                condition_img = condition_img.to(self.device).float()
+
+                # parse point cloud data
+                p = pointcloud[None].to(self.device).float()
+                n = pointcloud["normals"].to(self.device).float()
+                pointcloud = torch.cat([p, n], dim=1)
+
+                # forward propagation
+                out = self.model(depth, cam_K, cam_R, pointcloud, condition_img, img)
+
+                gt_imgs = img
+                gen_imgs = out["img_pred"]
+                loss = out["loss"]
+
+                # log images and loss
+                for gt, fake in zip(gt_imgs, gen_imgs):
+                    gt *= 255.0
+                    fake *= 255.0
+                    gt = gt.type(torch.uint8)
+                    fake = fake.type(torch.uint8)
+                    gt = gt.permute((1, 2, 0)).cpu().numpy()      # (C, H, W) -> (H, W, C)
+                    fake = fake.permute((1, 2, 0)).cpu().numpy()  # (C, H, W) -> (H, W, C)
+
+                    gt_img = Image.fromarray(gt)
+                    fake_img = Image.fromarray(fake)
+
+                    gt_img.save(os.path.join(out_dir, "real/{}.jpg".format(str(sample_idx).zfill(6))))
+                    fake_img.save(os.path.join(out_dir, "fake/{}.jpg".format(str(sample_idx).zfill(6))))
+
+                    sample_idx += 1
+
+                inf_total_loss += loss.item()
+
+            inf_total_loss /= (sample_idx)
+
+        print("[!] Total inference loss computed over {} samples: {}".format(sample_idx, inf_total_loss))
 
     def configure_optimizer(self) -> torch.optim.Optimizer:
         optimizer = optim.Adam(self.model.parameters(), lr=self.opts.lr)
@@ -182,7 +250,7 @@ class TextureFieldsConditionalTrainer(BaseTrainer):
     def configure_dataset(self) -> Tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
         if self.opts.dataset_type == "shapenet":
             dataset = ShapeNetSingleClassDataset(
-                "../research/data/shapenet/synthetic_cars_nospecular", img_size=128, num_pc_samples=2048
+                "./data/shapenet/synthetic_cars_nospecular", img_size=128, num_pc_samples=2048
             )
             print("[!] Dataset used: ShapeNet")
         else:
@@ -207,10 +275,15 @@ class TextureFieldsConditionalTrainer(BaseTrainer):
             batch_size=self.opts.batch_size,
             shuffle=True,
             num_workers=self.opts.num_workers,
+            drop_last=True,
         )
 
         test_loader = data.DataLoader(
-            self.test_dataset, batch_size=self.opts.test_dataset_size, shuffle=False
+            self.test_dataset, 
+            batch_size=self.opts.batch_size, 
+            shuffle=False,
+            num_workers=self.opts.num_workers // 2 if self.opts.num_workers > 1 else 1,
+            drop_last=True,
         )
 
         return train_loader, test_loader
